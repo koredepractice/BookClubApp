a2ad424969627db9707636222e0fcc6205f08785
Korede Osibodu <korede.osibodu@avanade.com>
2022-05-23 16:01:58 +0100

    Add project files.

---
+++ BookClub/Models/Books.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Models
+{
+    public class Books
+        //"Books" is the name of the first table in the database
+    {
+        [Key]
+            public int BookId { get; set; } //first column
+
+            [Required(ErrorMessage = "Title is required")]
+
+            [MinLength(2, ErrorMessage = "Length of description cannot be less than 2 characters")]
+            public string? Title { get; set; }
+
+            [Required(ErrorMessage = "Author is required")]
+            public string? Author { get; set; }
+
+            [Required(ErrorMessage = "ISBN Number is required")]
+            public int ISBN { get; set; }
+
+
+
+    }
+}
